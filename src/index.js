49ef329696f6080aa074e906eca0b87aceb0bdd3
Ken Urata <senseiurata@gmail.com>
2017-05-07 21:49:28 -0700

    Add redux devtools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, browserHistory } from 'react-router';
-import routes from './routes';
+import { createDevTools } from 'redux-devtools';
 import promise from 'redux-promise';
-
+import DockMonitor from 'redux-devtools-dock-monitor';
+import LogMonitor from 'redux-devtools-log-monitor';
+import routes from './routes';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const DevTools = createDevTools(
+  <DockMonitor toggleVisibilityKey='ctrl-h'
+             changePositionKey='ctrl-q'
+             defaultIsVisible={false}>
+    <LogMonitor theme='tomorrow' />
+  </DockMonitor>
+);
+
+const enhancer = compose(
+  applyMiddleware(promise),
+  DevTools.instrument(),
+);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={ browserHistory } routes={ routes } />
+  <Provider store={createStore(reducers, {}, enhancer)}>
+    <div>
+      <Router history={ browserHistory } routes={ routes } />
+      <DevTools />
+    </div>
   </Provider>
   , document.querySelector('.container'));
